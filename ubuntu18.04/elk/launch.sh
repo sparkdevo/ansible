eac67f2f3d242006d9812170342f26a4f70ac56d
spark <sparkdev@163.com>
2019-05-27 17:29:26 +0800

    add es single node install config

---
+++ ubuntu18.04/elk/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# please update inventory in conf/hosts file
+
+docker run --rm -it \
+    --mount type=bind,source=${PWD}/playbooks,target=/playbooks \
+    --mount type=bind,source=${HOME}/.ssh,target=/.ssh \
+    --mount type=bind,source=${PWD}/conf/ansible.cfg,target=/etc/ansible/ansible.cfg \
+    --mount type=bind,source=${PWD}/conf/hosts,target=/etc/ansible/hosts \
+    -w=/playbooks \
+    sparkdev/ansible:elk ansible-playbook -u nick --private-key=/.ssh/id_rsa es6.6.0.pb.yml
