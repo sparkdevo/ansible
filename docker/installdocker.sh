4b844a1fc04cce67fc7ee07eff84a3575ef912ce
nickli <lipower@live.com>
2019-07-14 19:13:43 +0800

    add node demo

---
+++ docker/installdocker.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run --rm \
+    --mount type=bind,source=${PWD},target=/workdir \
+	--mount type=bind,source=${HOME}/.ssh,target=/.ssh \
+	-w=/workdir \
+    sparkdev/ansible:1804-2.8 \
+    ansible-playbook --private-key=/.ssh/id_rsa -u nick \
+    docker.u1804.pb.yml
