7bf4c934f7b6dfd15b492af696e502330825c37a
nickli <lipower@live.com>
2019-07-18 15:14:31 +0800

    add .gitignore file

--- docker/runnodedemo.sh
+++ docker/runnodedemo.sh
@@ -2,8 +2,8 @@
 
 docker run --rm \
     --mount type=bind,source=${PWD},target=/workdir \
-	--mount type=bind,source=${HOME}/.ssh,target=/.ssh \
-	-w=/workdir \
+    --mount type=bind,source=${HOME}/.ssh,target=/.ssh \
+    -w=/workdir \
     sparkdev/ansible:1804-2.8 \
     ansible-playbook --private-key=/.ssh/id_rsa -u nick \
     container.u1804.pb.yml
